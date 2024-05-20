6bec3970ff73942f03207f22a89eee7a3b1b9831
CoolyaginAI <45224422+CoolyaginAI@users.noreply.github.com>
2024-05-20 11:45:26 +0300

    First

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        String newName;
+        Integer newAge;
+
+        System.out.println("Введите имя первого пользователя");
+        newName = in.nextLine();
+        System.out.println("Введите возраст первого пользователя");
+        newAge = in.nextInt();
+
+        User user1 = new User(newName, newAge);
+
+
+        System.out.println("Введите имя второго пользователя");
+        in.nextLine(); // РЕШЕНИЕ ПРОБЛЕМЫ СО СЧИТЫВАНИЕМ
+        newName = in.nextLine();
+        System.out.println("Введите возраст второго пользователя");
+        newAge = in.nextInt();
+
+        User user2 = new User(newName, newAge);
+
+        if (user1.getAge() == user2.getAge()) System.out.println("У них одинаковый возраст");
+         else if (user1.getAge() < user2.getAge()) System.out.println(user1.toString());
+               else System.out.println(user2.toString());
     }
 }
--- src/User.java
+++ src/User.java
@@ -3,6 +3,11 @@ public class User {
     private String name;
     private Integer age;
 
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public Integer getAge() {
         return age;
     }
